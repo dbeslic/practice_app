14a2f5d70ab886eaba8d15ed19dfaa284c157183
Dino Beslic <dbeslic@chicagobooth.edu>
2016-05-05 15:10:15 +0000

    test change

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ActiveRecord::Base
 
   belongs_to :user
 
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 }, presence :true 
+  validates :user_id, presence :true 
 
 end
